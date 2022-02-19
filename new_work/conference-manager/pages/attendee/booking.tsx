c40c0f11ac0c377cc1047aadde6494cd3abe7775
Samuel <sasamoah@uwo.ca>
2022-02-19 18:44:06 -0500

    Minor changes to bookings page

--- new_work/conference-manager/pages/attendee/booking.tsx
+++ new_work/conference-manager/pages/attendee/booking.tsx
@@ -3,6 +3,8 @@ import Conference_card from '../../components/Conference_card';
 import Background from '../../components/Background';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { signout } from '../../scripts/firebase';
+import Button from '../../components/Button';
 
 function booking() {
 
@@ -29,7 +31,6 @@ function booking() {
   
   console.log(conferenceCardData);
 
-
   return (
     <Background type='plain' color='#EEEEEE'>
       <div className='header'>
@@ -37,6 +38,18 @@ function booking() {
           <h1 className='title'> Conference Manager </h1>
           <h3 className='sub-title'> Booking </h3>
         </div>
+        <div className='buttons-container'>
+            <Button className='signout' title='back' color='#f86d4c' onclick={() => {
+              const lastIndex = window.location.href.lastIndexOf('/');
+              const home = window.location.href.slice(0, lastIndex);
+              lastIndex = home.lastIndexOf('/');
+              home = home.slice(0, lastIndex);
+              window.location.replace(home + '/role');
+            }}/>
+            <Button className='signout' title='sign out' color='#f8554c' onclick={() => {
+              signout();
+            }}/>
+          </div>
       </div>
         <div className='all-conferences'>
           {conferenceCardData.map((cardInfo) => {
